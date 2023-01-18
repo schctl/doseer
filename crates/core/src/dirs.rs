afb33f21b1075beaeb575c82b9dfb72cd19e9349
Sachin Cherian <sachinctl@protonmail.com>
2023-01-18 21:25:59 +0530

    fix project directory loading
    Using the package name would resolve the project name as `m7_core` since
    that's where its defined. Instead use `m7` directly.

--- crates/core/src/dirs.rs
+++ crates/core/src/dirs.rs
@@ -9,7 +9,7 @@ use crate::path::PathWrap;
 
 lazy_static::lazy_static! {
     pub static ref PROJECT: ProjectDirs
-                                = ProjectDirs::from("io", "schctl", env!("CARGO_PKG_NAME")).unwrap();
+                                = ProjectDirs::from("io", "schctl", "m7").unwrap();
 
     pub static ref BASE: BaseDirs = BaseDirs::new().unwrap();
 }
