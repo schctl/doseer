4f900875c71b80ef9509d0c2918d5f3a0396d224
Sachin Cherian <sachinctl@protonmail.com>
2023-01-20 18:08:33 +0530

    add some tracing options

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,30 @@ pub use side_bar::SideBar;
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+#[allow(unused)]
+fn setup_tracing() {
+    use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+
+    let mut env_filter = EnvFilter::from_default_env()
+        // Default -> WARN
+        .add_directive("m7=warn".parse().unwrap());
+
+    #[cfg(debug_assertions)]
+    {
+        // Debug mode -> INFO
+        env_filter = env_filter.add_directive("m7=info".parse().unwrap());
+    }
+    #[cfg(feature = "debug")]
+    {
+        // Full debug -> DEBUG
+        env_filter = env_filter
+            .add_directive("m7=debug".parse().unwrap())
+            .add_directive("iced=info".parse().unwrap());
+    }
+
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+}
+
 fn run() -> anyhow::Result<()> {
     let config = config::read_config().context("failed to get configuration")?;
     gui::Gui::run(Settings::with_flags(config))?;
@@ -38,7 +62,7 @@ fn run() -> anyhow::Result<()> {
 }
 
 fn main() {
-    // tracing_subscriber::fmt::init();
+    setup_tracing();
 
     if let Err(e) = run() {
         tracing::error!("{}", e);
