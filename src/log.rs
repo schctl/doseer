8579d08799eb18c30d42f49ef2760fc9e6c5a388
Sachin Cherian <sachinctl@protonmail.com>
2023-11-30 12:08:52 +0530

    change some logging configurations

--- src/log.rs
+++ src/log.rs
@@ -1,22 +1,30 @@
 //! Application wide logging configurations.
 
-use tracing_subscriber::filter::EnvFilter;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::filter::Targets;
+use tracing_subscriber::fmt;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+const fn lowest_log_level() -> LevelFilter {
+    if cfg!(feature = "debug") {
+        LevelFilter::DEBUG
+    } else {
+        LevelFilter::INFO
+    }
+}
 
 /// Initialize global tracing subscriber. Call only once.
 pub fn init_tracing() {
-    let mut env_filter = EnvFilter::from_default_env()
-        .add_directive("doseer=info".parse().unwrap())
-        .add_directive("iced=error".parse().unwrap());
+    let targets = Targets::default()
+        .with_default(lowest_log_level())
+        .with_target("iced", LevelFilter::WARN)
+        .with_target("wgpu", LevelFilter::OFF)
+        .with_target("naga", LevelFilter::OFF)
+        .with_target("cosmic_text", LevelFilter::INFO);
 
-    if cfg!(feature = "debug") {
-        env_filter = env_filter
-            .add_directive("doseer=debug".parse().unwrap())
-            .add_directive("iced=warn".parse().unwrap());
-    } else if cfg!(feature = "trace") {
-        env_filter = env_filter
-            .add_directive("doseer=trace".parse().unwrap())
-            .add_directive("iced=info".parse().unwrap());
-    }
-
-    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(targets)
+        .init();
 }
